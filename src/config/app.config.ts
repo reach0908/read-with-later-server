f0d2b4f4e5681821c8c2c36d49412c8c0dd04c98
reach0908 <wlsghkim0908@gmail.com>
2025-07-04 19:18:58 +0900

    feat(scraper): Puppeteer 기반의 브라우저 서비스 및 스크래퍼 모듈 추가

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
 	BASE_URL: process.env.BASE_URL || `http://localhost:${process.env.PORT || 4000}`,
 	CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
+	BROWSER_PATH: process.env.BROWSER_PATH || '/usr/bin/chromium-browser',
 }));
